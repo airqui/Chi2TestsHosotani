431cc81672019c8d155852cacd075a9438f22830
airqui <airqui@ific.uv.es>
2023-04-13 15:46:35 +0200

    dummy unc. for 500GeV

--- include/experimentalUnc.h
+++ include/experimentalUnc.h
@@ -1,4 +1,7 @@
-float r_stat[2][4]={{0.18,0.27,0.12,0.23},{}};//{{250 c-left, c-right, b-left, b-right},{500, same }}
-float afb_stat[2][4]={{0.38,0.52,0.24,0.7},{}};
-float r_syst[2][4]={{0.15,0.12,0.18,0.29},{}};
-float afb_syst[2][4]={{0.16,0.19,0.13,0.22},{}};
+//13th April 2023, 
+//250 GeV errors are correct (ILD ntoe in progress)
+//500 GeV errors are a wild guess
+float r_stat[2][4]={{0.18,0.27,0.12,0.23},{0.18*2.,0.27*2.,0.12*2.,0.2*2.}};//{{250 c-left, c-right, b-left, b-right},{500, same }}
+float afb_stat[2][4]={{0.38,0.52,0.24,0.7},{0.38*2.,0.52*2.,0.24*2.,0.7*2.}};
+float r_syst[2][4]={{0.15,0.12,0.18,0.29},{0.15,0.12,0.18,0.2}};
+float afb_syst[2][4]={{0.16,0.19,0.13,0.22},{0.16,0.19,0.13,0.22}};
